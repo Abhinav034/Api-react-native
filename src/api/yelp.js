ef914f422b6d9b42b0fb50c80a9329422a66509b
Abhinav Bhardwaj <abhinavbhardwaj034@gmail.com>
2020-11-08 15:20:31 -0500

    API key removed

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default axios.create({
 baseURL: 'https://api.yelp.com/v3/businesses',
 headers:{
-    Authorization: 'Bearer m0dUh2tosaW3AB4p84aWvRwfoUpMcIOhEVYsgcuuAYD_fO7zo4ZnpHDqzkMhvQqGux6lafhtHnmgpCJKBhKdJNzIQ0EKGnOwpWHbgoO2xyuR0BaHFgSrznDAlfGmX3Yx'
+    Authorization: 'Bearer '
 }
 })
