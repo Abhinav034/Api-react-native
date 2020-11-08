e94cc50110432ceebdef7893fd6559d724cdece0
Abhinav Bhardwaj <abhinavbhardwaj034@gmail.com>
2020-11-07 22:54:28 -0500

    Fixed minor issues

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default axios.create({
 baseURL: 'https://api.yelp.com/v3/businesses',
 headers:{
-    Authorization: 'Bearer YOUR API KEY (add single space after bearer)'
+    Authorization: 'Bearer API KEY HERE WITH SINGLE SPACE FROM Bearer'
 }
 })
--- src/screens/searchScreen.js
+++ src/screens/searchScreen.js
@@ -1,7 +1,8 @@
-import React , {useState} from 'react'
-import {View , Text, FlatList , Image} from 'react-native'
+import React , {useState , useEffect} from 'react'
+import {View , Text, FlatList , Image, StyleSheet} from 'react-native'
 import SearchBar from '../component/searchComp'
 import axios from '../api/yelp'
+var count = 0
 const SearchScreen = ()=>{
 
 const [search , userSearch] = useState('')
@@ -18,6 +19,45 @@ const fetchRequest = async ()=>{
      setResults(response.data.businesses)
 }
 
+useEffect(()=>{
+  initialRequest()
+})
+
+const initialRequest = async()=>{
+
+    if(count<1){
+        const response = await axios.get('/search' , {
+            params:{
+                limit:50,
+                term: 'shops',
+                location:'mississauga' 
+             }
+        })
+    
+        setResults(response.data.businesses)
+        count++
+
+    }
+}
+
+const styles = StyleSheet.create({
+    imageTag:{
+        height:200,
+         width:400,
+         
+    },
+    addressTag:{
+        fontSize: 20,
+        paddingHorizontal:5, 
+        marginVertical:10,
+    },
+    reviewTag:{
+        fontSize: 15,
+        paddingHorizontal:5, 
+        marginVertical:10,
+        color: '#c47900'
+    }
+})
 
 
 return <View>
@@ -26,9 +66,11 @@ return <View>
 data = {result}
 renderItem={({item})=>{
     return <View>
-<Image source={{uri:item.image_url}}  style={{height:100 , width:100}}/>
-<Text>{item.location.address1}</Text>
-    </View>
+<Image source={item.image_url?{uri:item.image_url}:{uri:'https://1080motion.com/wp-content/uploads/2018/06/NoImageFound.jpg.png'}}  style={styles.imageTag}/>
+<Text style={styles.addressTag}>{item.location.address1}</Text>
+<Text style={styles.reviewTag}>Average of:{item.rating} stars from {item.review_count} reviews </Text>
+
+ </View>
 
 
 }}
